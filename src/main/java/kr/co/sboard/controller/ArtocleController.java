2e7c8e5970100f1141f73410f0a33fed40b62197
kojunghyun <76957142+vam879@users.noreply.github.com>
2025-09-26 11:15:20 +0900

    github

---
+++ src/main/java/kr/co/sboard/controller/ArtocleController.java
@@ -0,0 +1,21 @@
+package kr.co.sboard.controller;
+
+
+@Controller
+public class ArtocleController {
+
+    @GetMapping("/article/modify")
+    public String modify() {
+
+    }
+    @GetMapping("/article/modify")
+    public String modify() {
+
+    }
+
+    @GetMapping("/article/user")
+    public String modify() {
+
+    }
+
+}
